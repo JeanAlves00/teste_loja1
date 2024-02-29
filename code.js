0363a416765ef16b696ea09f03541ce345a60930
JeanAlves00 <154983968+JeanAlves00@users.noreply.github.com>
2024-02-29 11:40:33 -0400

    destaques
    criando a seção de destaques

---
+++ code.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('.slider1').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      fade: true,
+      asNavFor: '.slider2'
+    });
+    $('.slider2').slick({
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      asNavFor: '.slider1',
+      dots: false,
+      centerMode: true,
+      focusOnSelect: true
+    });
+  });
+  
